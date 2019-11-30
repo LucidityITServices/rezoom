9a65164612e49eaecb002810805ea2bd22de2ce0
Laura <lmayers3@gmail.com>
2019-11-30 17:34:12 -0500

    second commit

--- login.php
+++ login.php
@@ -25,7 +25,7 @@ if(isset($_POST['submit']) && $_POST['submit']=="submit") {
         }
     }
     else {
-        //login failed
+        echo "Username and/or password incorrect.";
     }
 
     mysqli_close($link);
