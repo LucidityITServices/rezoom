de1b8543041bcbd9d631dffc961f190fb14ef91c
LucidityITServices <75534053+LucidityITServices@users.noreply.github.com>
2020-12-05 14:25:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,65 +1,165 @@
-<?php
-    session_start();
-?>
+
+<!DOCTYPE HTML>
+<!--
+	Prism by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
 <html>
-<meta charset="UTF-8">
-<head>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.js"></script>
-    <script src="js/slick/slick.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="js/slick/slick.css">
-    <link rel="stylesheet" href="js/slick/slick-theme.css">
+	<head>
+		<title>Prism by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+		    <header style="width:100%;">
+        <nav style="display:flex; justify-content:space-between;">
+            <a href="index.php">Home</a>
+            <div>
+                                            <a href="login.php">Login</a>
+                            <a href="register.php">Register</a>
+                                        <a href="faq.php">FAQ</a>
+            </div>
+        </nav>
+    </header>		<!-- Banner -->
+		<section id="banner">
+			<div class="inner split">
+				<section>
+					<h2>Welcome to Rezoom.</h2>
+				</section>
+				<section>
+					<p>We get it — applying to jobs is a pain.</p>
+					<p>Rezoom makes it easier to apply for jobs by letting you reformat your resume on the fly. No more juggling a dozen versions
+						of your resume — just input your info and you're good to go!</p>
+					<ul class="actions">
+						<li><a href="register.php" class="button special">Get started</a></li>
+					</ul>
+				</section>
+			</div>
+		</section>
 
-    <script>
-        $(document).ready(function() {
-            $('.slick').slick({
-                infinite: true,
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                dots:true,
-                arrows:true,
-                centerMode:true,
-            });
-        });
-    </script>
+		<!-- One -->
+			<section id="one" class="wrapper">
+				<div class="inner split">
+					<section>
+						<h2>Turbocharge your job search</h2>
+						<p>Job search advisors suggest tailoring your resume to each job that you're applying for. Save time by using Rezoom's customizable resume template system to create the perfect
+						resume for your job application.</p>
+						<ul class="actions">
+							<li><a href="about.php" class="button alt">Learn more</a></li>
+						</ul>
+					</section>
+					<section>
+						<ul class="checklist">
+							<li>8759 extra cat pictures taken</li>
+							<li>103 video games played</li>
+							<li>56 dinners with family</li>
+							<li>107 extra naps</li>
+						</ul>
+					</section>
+				</div>
+			</section>
 
-    <style>
-        body { padding: 0 10px; }
-        header { display:block; height:75px; }
-        nav a { display:inline-block; text-align:center; color:red; float:right; }
-        nav a:hover { text-decoration:none; color:darkred; }
-        nav a span { display:block; }
-        .slick { margin: 0 50px; }
-        .slick-prev:before, .slick-next:before { color:red; }
-    </style>
-</head>
-<body>
-    <?php include header.php ?>
-    <hr/>
-    <section id="about">
-        <h1>What's this all about?</h1>
-        <p>Most job hunting advice suggests formatting your resume according to the type of position that you're applying to.
-        Having multiple versions of your resume for different types of jobs is a pain.</p>
-        <p>Rezoom eliminates that, quickly and easily, by storing all the information you'd want to have on your resume, and 
-        letting you swap formatting templates on the fly.</p>
-        <a href="builder.php">Get started</a>
-    </section>
-    <section id="previews">
-        <h1>Previews</h1>
-        <div class="slick">
-            <img src="ph.gif">
-            <img src="ph.gif">
-            <img src="ph.gif">
-            <img src="ph.gif">
-            <img src="ph.gif">
-            <img src="ph.gif">
-            <img src="ph.gif">
-            <img src="ph.gif">
-            <img src="ph.gif">
-        </div>
-    </section>
-    <footer style="display:block">Built and managed by <a href="dcwebdev.net">dcwebdev.net</a></footer>
-</body>
+		<!-- Two -->
+			<section id="two" class="wrapper style2 alt">
+				<div class="inner">
+				    <h2>Templates</h2>
+					<div style="display:inline-flex; width:100%; flex-wrap:wrap; margin-bottom:20px;">
+						<div class="spotlight">
+							<div class="image">
+								<img src="images/pic01.jpg" alt="" />
+							</div>
+						</div>
+						<div class="spotlight">
+							<div class="image">
+								<img src="images/pic02.jpg" alt="" />
+							</div>
+						</div>
+						<div class="spotlight">
+							<div class="image">
+								<img src="images/pic03.jpg" alt="" />
+							</div>
+						</div>
+						<div class="spotlight">
+							<div class="image">
+								<img src="images/pic01.jpg" alt="" />
+							</div>
+						</div>
+						<div class="spotlight">
+							<div class="image">
+								<img src="images/pic02.jpg" alt="" />
+							</div>
+						</div>
+						<div class="spotlight">
+							<div class="image">
+								<img src="images/pic03.jpg" alt="" />
+							</div>
+						</div>
+						<div class="spotlight">
+							<div class="image">
+								<img src="images/pic01.jpg" alt="" />
+							</div>
+						</div>
+						<div class="spotlight">
+							<div class="image">
+								<img src="images/pic02.jpg" alt="" />
+							</div>
+						</div>
+						<div class="spotlight">
+							<div class="image">
+								<img src="images/pic03.jpg" alt="" />
+							</div>
+						</div>
+						<div class="spotlight">
+							<div class="image">
+								<img src="images/pic03.jpg" alt="" />
+							</div>
+						</div>
+					</div>
+					<ul class="actions special">
+						<li><a href="#" class="button alt">See All Templates</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- Contact -->
+			<section id="contact" class="wrapper">
+				<div class="inner split">
+					<section>
+						<ul class="info">
+							<li class="fa-twitter"><a href="#">twitter.com/untitled-tld</a></li>
+							<li class="fa-facebook"><a href="#">facebook.com/untitled-tld</a></li>
+							<li class="fa-instagram"><a href="#">instagram.com/untitled-tld</a></li>
+							<li class="fa-envelope"><a href="#">information@untitled.tld</a></li>
+						</ul>
+					</section>
+					<section>
+						<ul class="contact">
+							<li class="fa-twitter"><a href="#">twitter.com/untitled-tld</a></li>
+							<li class="fa-facebook"><a href="#">facebook.com/untitled-tld</a></li>
+							<li class="fa-instagram"><a href="#">instagram.com/untitled-tld</a></li>
+							<li class="fa-envelope"><a href="#">information@untitled.tld</a></li>
+													</ul>
+					</section>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="copyright">
+					&copy; Untitled. All rights reserved. Images: <a href="http://unsplash.com">Unsplash</a>. Design: <a href="http://templated.co">TEMPLATED</a>.
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="/js/jquery.min.js"></script>
+			<script src="js/skel.min.js"></script>
+			<script src="/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="/js/main.js"></script>
+
+	</body>
+</html>
