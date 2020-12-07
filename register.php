10b1d3d09ff451e98be2c5493363ce01f7f3c1ea
Laura <lmayers3@gmail.com>
2020-12-07 14:45:35 -0500

    final fix hopefully

--- register.php
+++ register.php
@@ -1,48 +1,39 @@
-<?
+<?php
+$error = "";
 if($_POST['submit']=="submit") {
     include("login-info.php");
     $link = new mysqli($host, $user, $pass, $db);
 
     if (!$link) {
-        echo "Error: Unable to connect to MySQL." . PHP_EOL;
-        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        echo "Something went wrong but we're on it." . PHP_EOL . "Go back to the <a href='https://rezoom.lucidity.net'>homepage</a>";
         exit;
     }
 
-    echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-    echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-    $query = "SELECT COUNT(*) FROM tblUsers WHERE username='".$_POST['username']."'";
+    $username = $_POST['username'];
+    $query = "SELECT * FROM tblusers WHERE username='$username'";
     $result = $link->query($query);
 
     if($result->num_rows==1) {
         $error = "That username is already in use.";
     }
-    else { 
-        $username = $_POST['username'];
+    else {
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-        $query = "INSERT INTO tblusers (username,`password`) VALUES ('$username','$password');";
-        $result = $link->query($query) or die("reg failed");
-        echo "Registration successful";
+        $query = "INSERT INTO tblusers (username,`password`) VALUES ('$username','$password')";
+        $result = $link->query($query);
     }
 
     mysqli_close($link);
 }
-else { echo "???"; }
 ?>
 
 <html>
-<?php
+    <?php
 		$title = "Rezoom - Sign Up";
 		include("head.php");
 	?>
 <body>
-    <?php
-        include("nav.php");
-        if(strlen($error)) { echo $error; }
-    ?>
+    <?php include("nav.php"); ?>
     <form action="register.php" method="post">
         <label for="username">Username: <input type="text" class="form-control" value="" name="username" id="username"></label>
         <label for="password">Password: <input type="password" class="form-control" value="" name="password" id="password"></label>
