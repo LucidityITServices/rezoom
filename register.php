0de4a858cfa3b2aacfbc12387ae3107a8081ec80
Laura <lmayers3@gmail.com>
2020-12-07 11:20:05 -0500

    password handling fix

--- register.php
+++ register.php
@@ -1,25 +1,27 @@
 <?
-if($_GET['submit']=="submit")
-    $link = new mysqli("127.0.0.1", "my_user", "my_password", "my_db");
+if($_POST['submit']=="submit") {
+    include("login-info.php");
+    $link = new mysqli($host, $user, $pass, $db);
 
     if (!$link) {
-        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        /*echo "Error: Unable to connect to MySQL." . PHP_EOL;
         echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;*/
         exit;
     }
 
+    /*
     echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-    echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+    echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;*/
 
-    $query = "SELECT COUNT(*) FROM tblUsers WHERE username='".$_GET['username']."'";
+    $query = "SELECT COUNT(*) FROM tblUsers WHERE username='".$_POST['username']."'";
     $result = $link->query($query);
 
     if($result->num_rows==1) {
-        //user already exists
+        $error = "That username is already in use.";
     }
     else { 
-        $query = "INSERT INTO tblUsers (username,password) VALUES (".$_GET['username'].",".$_GET['password'].");";
+        $query = "INSERT INTO tblUsers (username,`password`) VALUES ('".$_POST['username']."','".password_hash($_POST['password'],PASSWORD_DEFAULT)."');";
         $result = $link->query($query);
     }
 
@@ -28,6 +30,9 @@ if($_GET['submit']=="submit")
 ?>
 
 <html>
+<head>
+    <link rel="stylesheet" href="css/main.css">
+</head>
 <body>
     <header style="width:100%;" class="subpage-header">
         <nav style="display:flex; justify-content:space-between;">
@@ -39,7 +44,10 @@ if($_GET['submit']=="submit")
             </div>
         </nav>
     </header>
-    <form action="#" method="get">
+    <?php
+        if(strlen($error)) { echo $error; }
+    ?>
+    <form action="#" method="post">
         <input type="text" value="" name="username" id="username">
         <input type="text" value="" name="password" id="password">
         <button name="submit" value="submit">Submit</button>
